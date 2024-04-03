6129036f6e0bbfb1bc99a40c06c0204c86d3a173
Bingchen Zhao <zhaobc.gm@gmail.com>
2024-04-03 13:25:53 +0100

    update demo

--- README.md
+++ README.md
@@ -5,8 +5,28 @@ colorFrom: green
 colorTo: green
 sdk: gradio
 sdk_version: 4.25.0
-app_file: app.py
-pinned: false
+app_file: edit_app.py
+pinned: true
 ---
 
 Check out the configuration reference at https://huggingface.co/docs/hub/spaces-config-reference
+
+
+## BibTeX
+
+```
+@article{hqedit2024,
+  title={HQ-Edit: A High-Quality and High-Coverage Dataset for General Image Editing},
+  author={Mude Hui and Siwei Yang and Bingchen Zhao and Yichun Shi and Heng Wang and Peng Wang and Cihang Xie and Yuyin Zhou},
+  journal={arXiv preprint arXiv:xxxx.xxxxx},
+  year={2024}
+}
+
+@article{brooks2022instructpix2pix,
+  title={InstructPix2Pix: Learning to Follow Image Editing Instructions},
+  author={Brooks, Tim and Holynski, Aleksander and Efros, Alexei A},
+  journal={arXiv preprint arXiv:2211.09800},
+  year={2022}
+}
+```
+
---
+++ edit_app.py
@@ -0,0 +1,194 @@
+from __future__ import annotations
+
+import math
+import random
+
+import gradio as gr
+import torch
+from PIL import Image, ImageOps
+from diffusers import StableDiffusionInstructPix2PixPipeline, EulerAncestralDiscreteScheduler
+
+
+help_text = """
+If you're not getting what you want, there may be a few reasons:
+1. Is the image not changing enough? Your Image CFG weight may be too high. This value dictates how similar the output should be to the input. It's possible your edit requires larger changes from the original image, and your Image CFG weight isn't allowing that. Alternatively, your Text CFG weight may be too low. This value dictates how much to listen to the text instruction. The default Image CFG of 1.5 and Text CFG of 7.5 are a good starting point, but aren't necessarily optimal for each edit. Try:
+    * Decreasing the Image CFG weight, or
+    * Increasing the Text CFG weight, or
+2. Conversely, is the image changing too much, such that the details in the original image aren't preserved? Try:
+    * Increasing the Image CFG weight, or
+    * Decreasing the Text CFG weight
+3. Try generating results with different random seeds by setting "Randomize Seed" and running generation multiple times. You can also try setting "Randomize CFG" to sample new Text CFG and Image CFG values each time.
+4. Rephrasing the instruction sometimes improves results (e.g., "turn him into a dog" vs. "make him a dog" vs. "as a dog").
+5. Increasing the number of steps sometimes improves results.
+6. Do faces look weird? The Stable Diffusion autoencoder has a hard time with faces that are small in the image. Try:
+    * Cropping the image so the face takes up a larger portion of the frame.
+"""
+
+
+example_instructions = [
+    "Make it a picasso painting",
+    "as if it were by modigliani",
+    "convert to a bronze statue",
+    "Turn it into an anime.",
+    "have it look like a graphic novel",
+    "make him gain weight",
+    "what would he look like bald?",
+    "Have him smile",
+    "Put him in a cocktail party.",
+    "move him at the beach.",
+    "add dramatic lighting",
+    "Convert to black and white",
+    "What if it were snowing?",
+    "Give him a leather jacket",
+    "Turn him into a cyborg!",
+    "make him wear a beanie",
+]
+
+# model_id = "timbrooks/instruct-pix2pix"
+model_id = "MudeHui/ip2p-warp-gpt4v"
+
+def main():
+    pipe = StableDiffusionInstructPix2PixPipeline.from_pretrained(model_id, torch_dtype=torch.float16, safety_checker=None).to("cuda")
+    pipe.scheduler = EulerAncestralDiscreteScheduler.from_config(pipe.scheduler.config)
+    example_image = Image.open("imgs/example.jpg").convert("RGB")
+
+    def load_example(
+        steps: int,
+        randomize_seed: bool,
+        seed: int,
+        randomize_cfg: bool,
+        text_cfg_scale: float,
+        image_cfg_scale: float,
+    ):
+        example_instruction = random.choice(example_instructions)
+        return [example_image, example_instruction] + generate(
+            example_image,
+            example_instruction,
+            steps,
+            randomize_seed,
+            seed,
+            randomize_cfg,
+            text_cfg_scale,
+            image_cfg_scale,
+        )
+
+    def generate(
+        input_image: Image.Image,
+        instruction: str,
+        steps: int,
+        randomize_seed: bool,
+        seed: int,
+        randomize_cfg: bool,
+        text_cfg_scale: float,
+        image_cfg_scale: float,
+    ):
+        seed = random.randint(0, 100000) if randomize_seed else seed
+        text_cfg_scale = round(random.uniform(6.0, 9.0), ndigits=2) if randomize_cfg else text_cfg_scale
+        image_cfg_scale = round(random.uniform(1.2, 1.8), ndigits=2) if randomize_cfg else image_cfg_scale
+
+        width, height = input_image.size
+        factor = 512 / max(width, height)
+        factor = math.ceil(min(width, height) * factor / 64) * 64 / min(width, height)
+        width = int((width * factor) // 64) * 64
+        height = int((height * factor) // 64) * 64
+        input_image = ImageOps.fit(input_image, (width, height), method=Image.Resampling.LANCZOS)
+
+        if instruction == "":
+            return [input_image, seed]
+
+        generator = torch.manual_seed(seed)
+        edited_image = pipe(
+            instruction, image=input_image,
+            guidance_scale=text_cfg_scale, image_guidance_scale=image_cfg_scale,
+            num_inference_steps=steps, generator=generator,
+        ).images[0]
+        return [seed, text_cfg_scale, image_cfg_scale, edited_image]
+
+    def reset():
+        return [0, "Randomize Seed", 1371, "Fix CFG", 7.5, 1.5, None]
+
+    with gr.Blocks() as demo:
+        gr.HTML("""<h1 style="font-weight: 900; margin-bottom: 7px;">
+   InstructPix2Pix: Learning to Follow Image Editing Instructions
+</h1>
+<p>For faster inference without waiting in queue, you may duplicate the space and upgrade to GPU in settings.
+<br/>
+<a href="https://huggingface.co/spaces/timbrooks/instruct-pix2pix?duplicate=true">
+<img style="margin-top: 0em; margin-bottom: 0em" src="https://bit.ly/3gLdBN6" alt="Duplicate Space"></a>
+<p/>""")
+        with gr.Row():
+            with gr.Column(scale=1, min_width=100):
+                generate_button = gr.Button("Generate")
+            with gr.Column(scale=1, min_width=100):
+                load_button = gr.Button("Load Example")
+            with gr.Column(scale=1, min_width=100):
+                reset_button = gr.Button("Reset")
+            with gr.Column(scale=3):
+                instruction = gr.Textbox(lines=1, label="Edit Instruction", interactive=True)
+
+        with gr.Row():
+            input_image = gr.Image(label="Input Image", type="pil", interactive=True, height=512, width=512)
+            edited_image = gr.Image(label=f"Edited Image", type="pil", interactive=False, height=512, width=512)
+            # input_image.style(height=512, width=512)
+            # edited_image.style(height=512, width=512)
+
+        with gr.Row():
+            steps = gr.Number(value=20, precision=0, label="Steps", interactive=True)
+            randomize_seed = gr.Radio(
+                ["Fix Seed", "Randomize Seed"],
+                value="Randomize Seed",
+                type="index",
+                show_label=False,
+                interactive=True,
+            )
+            seed = gr.Number(value=1371, precision=0, label="Seed", interactive=True)
+            randomize_cfg = gr.Radio(
+                ["Fix CFG", "Randomize CFG"],
+                value="Fix CFG",
+                type="index",
+                show_label=False,
+                interactive=True,
+            )
+            text_cfg_scale = gr.Number(value=7.0, label=f"Text CFG", interactive=True)
+            image_cfg_scale = gr.Number(value=1.5, label=f"Image CFG", interactive=True)
+
+        gr.Markdown(help_text)
+
+        load_button.click(
+            fn=load_example,
+            inputs=[
+                steps,
+                randomize_seed,
+                seed,
+                randomize_cfg,
+                text_cfg_scale,
+                image_cfg_scale,
+            ],
+            outputs=[input_image, instruction, seed, text_cfg_scale, image_cfg_scale, edited_image],
+        )
+        generate_button.click(
+            fn=generate,
+            inputs=[
+                input_image,
+                instruction,
+                steps,
+                randomize_seed,
+                seed,
+                randomize_cfg,
+                text_cfg_scale,
+                image_cfg_scale,
+            ],
+            outputs=[seed, text_cfg_scale, image_cfg_scale, edited_image],
+        )
+        reset_button.click(
+            fn=reset,
+            inputs=[],
+            outputs=[steps, randomize_seed, seed, randomize_cfg, text_cfg_scale, image_cfg_scale, edited_image],
+        )
+
+    demo.queue()
+    demo.launch(share=True, max_threads=1)
+
+
+if __name__ == "__main__":
+    main()
